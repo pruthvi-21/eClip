8be1e1328477ad63275707f7f9b4f3e23820736f
pruthvi-21 <pruthvishankar397@gmail.com>
2021-09-09 00:03:11 +0530

    Added a toolbar and fab in home page

--- app/src/main/java/com/ps/eclip/HomeActivity.kt
+++ app/src/main/java/com/ps/eclip/HomeActivity.kt
@@ -2,10 +2,14 @@ package com.ps.eclip
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
     }
 }
